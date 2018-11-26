10a484a0276514bc484f386cb98d5608be1d2a5a
RICARDO MAGNO DO NASCIMENTO FERREIRA 113112300 <ricardomnf@lcc3-108.lcc.ufcg.edu.br>
2018-11-26 09:05:47 -0300

    router

--- receita_facil_front/src/App.js
+++ receita_facil_front/src/App.js
@@ -4,8 +4,10 @@ import './App.css';
 import Recipe from './Recipe';
 import GridRecipes from './GridRecipes';
 import TitlebarGridList from './TitlebarGridList';
+import AutoGrid from './AutoGrid';
+import AdvancedGridList from './AdvancedGridList';
 import tileData from './tileData';
-
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
   constructor() {
@@ -20,15 +22,22 @@ class App extends Component {
 
   render() {
     return (
+
       <div className="App">
         <Greeting greeting="Bem vindo ao Receita Facil" />
         <header className="App-header">
                 <img src={logo} className="Receita-logo" alt="logo" />
         </header>
         <body className="App-body">
+          <Router>
+            <div>
+              <Route exact path="/" component={Home} />
+              <Route path="/grid" component={Grid} />
+              <Route path="/recipe" component={rRecipe} />
+            </div>
+          </Router>
           <GridRecipes className="recipe" recipe={this.state.recipe}/>
           <Recipe className="recipe" recipe={this.state.recipe}/>
-          <TitlebarGridList/>
         </body>
         <footer className="App-footer">
                 <p>Footer</p>
@@ -39,5 +48,8 @@ class App extends Component {
 }
 const Greeting = props => <h1>{props.greeting}</h1>;
 
+const Home = () => <h2>Home</h2>;
+const Grid = () => <GridRecipes/>;
+const rRecipe = () => <Recipe/>;
 
 export default App;
---
+++ receita_facil_front/src/tileData.js
@@ -0,0 +1,69 @@
+const tileData = [
+    {
+      img: '/static/images/breakfast.jpg',
+      title: 'Breakfast',
+      author: 'jill111',
+      cols: 2,
+      featured: true,
+    },
+    {
+      img: '/static/images/burgers.jpg',
+      title: 'Tasty burger',
+      author: 'director90',
+    },
+    {
+      img: '/static/images/camera.jpg',
+      title: 'Camera',
+      author: 'Danson67',
+    },
+    {
+      img: '/static/images/morning.jpg',
+      title: 'Morning',
+      author: 'fancycrave1',
+      featured: true,
+    },
+    {
+      img: '/static/images/hats.jpg',
+      title: 'Hats',
+      author: 'Hans',
+    },
+    {
+      img: '/static/images/honey.jpg',
+      title: 'Honey',
+      author: 'fancycravel',
+    },
+    {
+      img: '/static/images/vegetables.jpg',
+      title: 'Vegetables',
+      author: 'jill111',
+      cols: 2,
+    },
+    {
+      img: '/static/images/plant.jpg',
+      title: 'Water plant',
+      author: 'BkrmadtyaKarki',
+    },
+    {
+      img: '/static/images/mushroom.jpg',
+      title: 'Mushrooms',
+      author: 'PublicDomainPictures',
+    },
+    {
+      img: '/static/images/olive.jpg',
+      title: 'Olive oil',
+      author: 'congerdesign',
+    },
+    {
+      img: '/static/images/star.jpg',
+      title: 'Sea star',
+      cols: 2,
+      author: '821292',
+    },
+    {
+      img: '/static/images/bike.jpg',
+      title: 'Bike',
+      author: 'danfador',
+    },
+  ];
+  
+  export default tileData;
